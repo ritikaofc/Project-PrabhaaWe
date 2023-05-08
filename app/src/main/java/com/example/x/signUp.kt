8d076a0f69c50ede6c6b931297d59959abd22165
Yukta-Somani <1ds20is125@dsce.edu.in>
2023-05-08 22:34:43 +0530

    realtimedatabase

--- app/src/main/java/com/example/x/signUp.kt
+++ app/src/main/java/com/example/x/signUp.kt
@@ -9,6 +9,8 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class signUp : AppCompatActivity() {
 
@@ -17,6 +19,7 @@ class signUp : AppCompatActivity() {
     private lateinit var edtPassword: EditText
     private  lateinit var btnSignUp: Button
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var db:DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,31 +36,41 @@ class signUp : AppCompatActivity() {
         edtPassword=findViewById(R.id.password)
         btnSignUp=findViewById(R.id.btn_signup)
 
+
         btnSignUp.setOnClickListener{
             val email= edtEmail.text.toString()
             val password= edtPassword.text.toString()
+            val name=edtName.text.toString()
 
             //function to signup the user
-            signUp(email,password)
+            signUp(email,password,name)
         }
 
     }
 
-    private fun signUp(email: String, password: String) {
+    private fun signUp(email: String, password: String,name:String) {
         //Logic of creating user
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
+                    // Sign up success, update UI with the signed-in user's information
+                    addUserToDatabase(email,password,name)
                     val intent= Intent(this,MainActivity::class.java)
                     finish()
                     startActivity(intent)
 
                 } else {
-                    // If sign in fails, display a message to the user.
+                    // If sign up fails, display a message to the user.
                     Toast.makeText(this,"Some Error Occurred",Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun addUserToDatabase(email: String, password: String, name: String) {
+        db=FirebaseDatabase.getInstance().reference
+        Toast.makeText(this,"adding data to user",Toast.LENGTH_SHORT).show()
+        db.child("Users").child(email).child("signUp").setValue(signUpUser(name, email, password))
+        Toast.makeText(this,"added data to user",Toast.LENGTH_SHORT).show()
     }
 
 }
